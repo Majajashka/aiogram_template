7b893dcfc2e197ff6bde3133132ce320b94b93ae
majaja <majajashka@hotmail.com>
2024-05-31 03:47:26 +0400

    initial commit

---
+++ app/bot/keyboards/set_menu.py
@@ -0,0 +1,12 @@
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [
+        BotCommand(command='/admin_list',
+                   description='Список админов')
+    ]
+
+    await bot.delete_my_commands()
+    return bot.set_my_commands(main_menu_commands)
