922ac5a09fd7c5893ca0c5f01751a1c4e6cf737a
majaja <majajashka@hotmail.com>
2024-10-22 00:17:59 +0400

    feat: add database and redis config providing via IoC

--- app/infrastructure/di/config.py
+++ app/infrastructure/di/config.py
@@ -2,6 +2,8 @@ from dishka import Provider, Scope, provide
 
 from app.bot.config.model.bot import BotConfig
 from app.bot.config.parser.main import parse_bot_config
+from app.infrastructure.database.configs.models import DatabaseConfig, RedisConfig
+from app.infrastructure.database.configs.parsers import load_database_config, load_redis_config
 
 
 class ConfigProvider(Provider):
@@ -10,3 +12,11 @@ class ConfigProvider(Provider):
     @provide
     async def provide_bot_config(self) -> BotConfig:
         return parse_bot_config()
+
+    @provide
+    def get_database_config(self) -> DatabaseConfig:
+        return load_database_config()
+
+    @provide
+    def get_redis_config(self) -> RedisConfig:
+        return load_redis_config()
